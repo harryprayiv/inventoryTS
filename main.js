00b1d5dd94154a6de98752cef069ded82b47d9f6
harryprayiv <harryprayiv@gmail.com>
2023-04-21 20:07:59 -0400

    good first progress

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
-// main.ts
-import inventoryData from './inventory.json';
-const inventoryDiv = document.getElementById('inventory');
-function createNestedList(data, listElement) {
-    for (const key in data) {
-        const listItem = document.createElement('li');
-        listItem.textContent = key;
-        const nestedList = document.createElement('ul');
-        listItem.appendChild(nestedList);
-        listElement.appendChild(listItem);
-        createNestedList(data[key], nestedList);
-    }
+const loadMenuButton = document.getElementById('loadMenu');
+const mainCategoryElement = document.getElementById('mainCategory');
+const subCategoryElement = document.getElementById('subCategory');
+const itemElement = document.getElementById('item');
+loadMenuButton.addEventListener('click', () => {
+    fetch('./inventory.json')
+        .then(response => response.json())
+        .then(menuData => {
+        populateMainCategories(menuData);
+    })
+        .catch(error => console.error('Error fetching menu data:', error));
+});
+function populateMainCategories(menuData) {
+    mainCategoryElement.innerHTML = ''; // Clear the previous menu items
+    Object.keys(menuData).forEach(key => {
+        const optionItem = document.createElement('option');
+        optionItem.textContent = key;
+        mainCategoryElement.appendChild(optionItem);
+    });
+    mainCategoryElement.style.display = 'inline';
+    mainCategoryElement.addEventListener('change', () => {
+        populateSubCategories(menuData[mainCategoryElement.value]);
+    });
+    populateSubCategories(menuData[mainCategoryElement.value]);
 }
-if (inventoryDiv) {
-    const list = document.createElement('ul');
-    createNestedList(inventoryData, list);
-    inventoryDiv.appendChild(list);
+function populateSubCategories(subCategories) {
+    subCategoryElement.innerHTML = ''; // Clear the previous menu items
+    Object.keys(subCategories).forEach(key => {
+        const optionItem = document.createElement('option');
+        optionItem.textContent = key;
+        subCategoryElement.appendChild(optionItem);
+    });
+    subCategoryElement.style.display = 'inline';
+    subCategoryElement.addEventListener('change', () => {
+        populateItems(subCategories[subCategoryElement.value]);
+    });
+    populateItems(subCategories[subCategoryElement.value]);
+}
+function populateItems(items) {
+    itemElement.innerHTML = ''; // Clear the previous menu items
+    Object.keys(items).forEach(key => {
+        const optionItem = document.createElement('option');
+        optionItem.textContent = key;
+        itemElement.appendChild(optionItem);
+    });
+    itemElement.style.display = 'inline';
 }
--- main.ts
+++ main.ts
@@ -1,21 +1,56 @@
-// main.ts
-import inventoryData from './inventory.json';
+const loadMenuButton = document.getElementById('loadMenu') as HTMLButtonElement;
+const mainCategoryElement = document.getElementById('mainCategory') as HTMLSelectElement;
+const subCategoryElement = document.getElementById('subCategory') as HTMLSelectElement;
+const itemElement = document.getElementById('item') as HTMLSelectElement;
 
-const inventoryDiv = document.getElementById('inventory');
+loadMenuButton.addEventListener('click', () => {
+  fetch('./inventory.json')
+    .then(response => response.json())
+    .then(menuData => {
+      populateMainCategories(menuData);
+    })
+    .catch(error => console.error('Error fetching menu data:', error));
+});
 
-function createNestedList(data: any, listElement: HTMLUListElement) {
-  for (const key in data) {
-    const listItem = document.createElement('li');
-    listItem.textContent = key;
-    const nestedList = document.createElement('ul');
-    listItem.appendChild(nestedList);
-    listElement.appendChild(listItem);
-    createNestedList(data[key], nestedList);
-  }
+function populateMainCategories(menuData: any) {
+  mainCategoryElement.innerHTML = ''; // Clear the previous menu items
+  Object.keys(menuData).forEach(key => {
+    const optionItem = document.createElement('option');
+    optionItem.textContent = key;
+    mainCategoryElement.appendChild(optionItem);
+  });
+
+  mainCategoryElement.style.display = 'inline';
+  mainCategoryElement.addEventListener('change', () => {
+    populateSubCategories(menuData[mainCategoryElement.value]);
+  });
+
+  populateSubCategories(menuData[mainCategoryElement.value]);
 }
 
-if (inventoryDiv) {
-  const list = document.createElement('ul');
-  createNestedList(inventoryData, list);
-  inventoryDiv.appendChild(list);
+function populateSubCategories(subCategories: any) {
+  subCategoryElement.innerHTML = ''; // Clear the previous menu items
+  Object.keys(subCategories).forEach(key => {
+    const optionItem = document.createElement('option');
+    optionItem.textContent = key;
+    subCategoryElement.appendChild(optionItem);
+  });
+
+  subCategoryElement.style.display = 'inline';
+  subCategoryElement.addEventListener('change', () => {
+    populateItems(subCategories[subCategoryElement.value]);
+  });
+
+  populateItems(subCategories[subCategoryElement.value]);
+}
+
+function populateItems(items: any) {
+  itemElement.innerHTML = ''; // Clear the previous menu items
+  Object.keys(items).forEach(key => {
+    const optionItem = document.createElement('option');
+    optionItem.textContent = key;
+    itemElement.appendChild(optionItem);
+  });
+
+  itemElement.style.display = 'inline';
 }
